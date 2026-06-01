efd6aa5a6eb9f210e0de1223ab22275e62b33e09
haddjs <nadirhexonyte@gmail.com>
2026-06-02 00:19:32 +0700

    chore: resolve import in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import "dotenv/config"
 import app from "./server"
 import "./config/db"
-import "./config/cloudinary"
 
 app.listen(3000, () => {
     console.log("server is running on port 3000");
