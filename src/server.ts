01c794c4c9bd28234f17f2c37bbe4c420366dec6
haddjs <nadirhexonyte@gmail.com>
2026-06-07 23:38:28 +0700

    feat: adding global error handler

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
-import mainRouter from './routes/index'
+import { errorHandler } from "./middleware/errorHandler";
+import mainRouter from "./routes/index";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api', mainRouter)
+app.use("/api", mainRouter);
+
+app.use(errorHandler);
 
 export default app;
